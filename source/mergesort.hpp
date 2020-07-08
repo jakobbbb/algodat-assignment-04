4d3a5f96fcbff99dba5b52fc933567a8d7a13a89
Hendrik Kopplow <hendrik.kopplow@uni-weimar.de>
2020-07-08 20:57:09 +0200

    mergesort implemented

--- source/mergesort.hpp
+++ source/mergesort.hpp
@@ -5,11 +5,47 @@
 #include "point.hpp"
 
 template <typename Comparision>
-std::vector<point> mergesort(std::vector<point> const& c, Comparision less) {
-  // TODO
-  auto r{c};
-  std::sort(std::begin(r), std::end(r), less);
-  return r;
+std::vector<point> mergesort(std::vector<point> const& c, Comparision less_eq) {
+	if(c.size()<=1) {
+    return c;
+	}
+	else {
+          std::size_t m = c.size() / 2;
+          auto c_l = std::vector<point>(c.begin(), c.begin() + m);
+          auto c_r = std::vector<point>(c.begin() + m, c.end());
+          mergesort(c_l, less_eq);
+          mergesort(c_r, less_eq);
+	}
+}
+template <typename Comparision>
+std::vector<point> merge(std::vector<point> const& c_l,
+    std::vector<point> const& c_r,
+    Comparision less_eq) {
+  std::size_t size = c_l.size() + c_r.size();
+  auto c = std::vector<point>(size);
+  std::size_t firstPos = 0;
+  std::size_t secondPos = 0;
+  std::size_t targetPos = 0;
+  while (firstPos < c_l.size() || secondPos < c_r.size()) {
+    if (firstPos >= c_l.size()) {
+      c[targetPos] = c_r[secondPos];
+      secondPos++;
+    } else if (secondPos >= c_r.size()) {
+      c[targetPos] = c_l[firstPos];
+      firstPos++;
+    } else if (less_eq(c_l[firstPos], c_r[secondPos])) {
+      c[targetPos] = c_l[firstPos];
+      firstPos++;
+    } else {
+      c[targetPos] = c_r[secondPos];
+      secondPos++;
+    }
+    targetPos++;
+  }
+  
+  
+  
+  return c;
 }
 
 std::vector<point> mergesort_x(std::vector<point> const& c);
