1d3a54bc768bb0649dc7e0b0ad0d4540cee091c2
Jakob Ruckel <jakob@ruckel.de>
2020-07-08 18:38:07 +0200

    example: output gnuplot script

--- source/example.cpp
+++ source/example.cpp
@@ -3,14 +3,22 @@
 
 int main() {
   auto points = random_points(30, {0.f, 0.f}, {1.f, 1.f});
+  std::cout << "# gnuplot script\nunset key\nset term png\n"
+            << "set output 'plot.png'\n"
+            << "plot '-', '-' w l\n";
   print_points(std::cout, points);
+  std::cout << "e\n";
 
   auto n = closest_naive(points);
-  std::cout << "Closest points (naive):\t" << n.first << " " << n.second
-            << " (d=" << distance(n) << ")\n";
-
   auto c = closest(points);
-  std::cout << "Closest points (di&co):\t" << c.first << " " << c.second
+
+  std::cout << c.first.x << " " << c.first.y << "\n"
+            << c.second.x << " " << c.second.y << "\n";
+  std::cout << "e\n";
+
+  std::cout << "# Closest points (naive): " << n.first << " " << n.second
+            << " (d=" << distance(n) << ")\n";
+  std::cout << "# Closest points (di&co): " << c.first << " " << c.second
             << " (d=" << distance(c) << ")\n";
   return 0;
 }
