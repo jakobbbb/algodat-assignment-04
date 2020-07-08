7fbc6fab951b62d189f39bec6e397ee48dd57a0c
Jakob Ruckel <jakob@ruckel.de>
2020-07-08 18:51:58 +0200

    example: add optional parameter for no. of points

--- source/example.cpp
+++ source/example.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 #include "closest.hpp"
 
-int main() {
-  auto points = random_points(30, {0.f, 0.f}, {1.f, 1.f});
+int main(int argc, char** argv) {
+  int n_points = 30;
+  if (argc > 1) {
+    try {
+      n_points = std::stoi(argv[1]);
+    } catch (...) {
+      std::cerr << "Warning:  Ignoring provided number of points."
+                << "Defaulting to " << n_points;
+    }
+  }
+  auto points = random_points(n_points);
   std::cout << "# gnuplot script\nunset key\nset term png\n"
             << "set output 'plot.png'\n"
             << "plot '-', '-' w l\n";
