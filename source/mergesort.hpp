f799f767a0438e945c94d9e237cae8d763ac6cfa
Jakob Ruckel <jakob@ruckel.de>
2020-07-08 21:08:28 +0200

    fix mergesort (1/2)
    Note: Out `mergesort` _returns_ the vector and doesn't modify it
    in-place!

--- source/mergesort.hpp
+++ source/mergesort.hpp
@@ -12,8 +12,8 @@ std::vector<point> mergesort(std::vector<point> const& c, Comparision less_eq) {
     std::size_t m = c.size() / 2;
     auto c_l = std::vector<point>(c.cbegin(), c.cbegin() + m);
     auto c_r = std::vector<point>(c.cbegin() + m, c.cend());
-    mergesort(c_l, less_eq);
-    mergesort(c_r, less_eq);
+    c_l = mergesort(c_l, less_eq);
+    c_r = mergesort(c_r, less_eq);
   }
 }
 template <typename Comparision>
